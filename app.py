33b0f2a0d877881f7abf7d4b059c23831e795650
asquires11 <50277110+asquires11@users.noreply.github.com>
2020-05-13 09:17:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 
 import pandas as pd
-url_1=('my_evidence.csv') #this is model data
+url_1=('https://github.com/asquires11/flying-dog-beers/blob/master/my_evidence.csv') #this is model data
 df = pd.read_csv(url_1) # csv to pandas
 df = df[['tweet', 'Predicted Name', 'Predicted Confidence']] 
 
 PAGE_SIZE=10
 
-df_2=('finalmydata.csv')# this is location data for members AND has number of members per county
+df_2=('https://github.com/asquires11/flying-dog-beers/blob/master/finalmydata.csv')# this is location data for members AND has number of members per county
 df_2=pd.read_csv(df_2)
 
 
-url_4='/Users/annikasquires/Desktop/Desktop - Annika’s MacBook Pro/export_dataframe_final.xlsx'
+url_4='https://github.com/asquires11/flying-dog-beers/blob/master/export_dataframe_final.xlsx'
 url_4=pd.read_excel(url_4) #this is full data with locations and messages
 
 url_5=df.merge(url_4, left_on='tweet', right_on='msg_post') #merge of model and location etc
 
 
 
-url_5.to_csv('final_export_combined.csv')
+url_5.to_csv('https://github.com/asquires11/flying-dog-beers/blob/master/final_export_combined.csv')
 
 url_6=url_5[['tweet','Predicted Name','Predicted Confidence','msg_date','name','city',"state_name"]]
 
