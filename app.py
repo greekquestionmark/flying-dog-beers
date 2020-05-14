6400285d477b02e1260c1922bcc3c58a2e492362
Tekrin <greekquestionmark@users.noreply.github.com>
2020-05-14 15:52:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 
 import pandas as pd
 # let's get our file sources
-fo1 = open("my_evidence.csv") #opens for reading
-fo2 = open("finalmydata.csv") #opens for reading
-fo3 = open("export_dataframe_final.xlsx","rb")
-fo4 = open("final_export_combined.csv")
+fo1 = open("./my_evidence.csv") #opens for reading
+fo2 = open("./finalmydata.csv") #opens for reading
+fo3 = open("./export_dataframe_final.xlsx","rb")
+fo4 = open("./final_export_combined.csv")
 
 file1 = fo1.read()
 file2 = fo2.read()
