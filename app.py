a9cc7f92e54adf83a269d5fa2f6641ec03bda879
Tekrin <greekquestionmark@users.noreply.github.com>
2020-05-14 17:12:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 ####IMPORTS####
 import pandas as pd
-import plotly.graph_objects as go
 import dash
+import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
