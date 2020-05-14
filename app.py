d35fb903869771b945728c18342d0900edc268f7
Tekrin <greekquestionmark@users.noreply.github.com>
2020-05-14 17:14:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -446,12 +446,12 @@ def gen_table(dropdown_value):
     [Input("open", "n_clicks"), Input("close", "n_clicks")],
     [State("modal", "is_open")],
 )
+
 def toggle_modal(n1, n2, is_open):
     if n1 or n2:
         return not is_open
     return is_open
 
 
-
 if __name__ == '__main__':
-    app.run_server(host='127.0.0.1',debug=False)
+    app.run_server(host='localhost',debug=False)
